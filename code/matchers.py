5738075d57a2dfeb756474b0f46ba79c9ce10de4
Maud <maudmcok001@gmail.com>
2018-03-17 11:34:36 +0100

    Update
    Update and refacto

--- code/matchers.py
+++ code/matchers.py
@@ -3,28 +3,22 @@ import numpy as np
 
 class matchers:
 	def __init__(self):
-		#self.surf = cv2.xfeatures2d.SURF_create()
-		#FLANN_INDEX_KDTREE = 0
-		#index_params = dict(algorithm=0, trees=5)
-		#search_params = dict(checks=50)
-		#self.flann = cv2.FlannBasedMatcher(index_params, search_params)
+		self.surf = cv2.xfeatures2d.SURF_create()
+		FLANN_INDEX_KDTREE = 0
+		index_params = dict(algorithm=0, trees=5)
+		search_params = dict(checks=50)
+		self.flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-		#using ORB instead of SURF to speed up the process
-		# and using Brute Force Matcher instead of FLANN. The latter works only with SURF or SIFT and BF is faster and accurate for this
-		self.orb = cv2.ORB()
-
-		self.bf = cv2.BFMatcher()
-	
 	def match(self, i1, i2, direction=None):
-		imageSet1 = self.getFeatures(i1)
-		imageSet2 = self.getFeatures(i2)
-		print "Direction : ", direction
-		matches = self.bf.knnMatch(
+		imageSet1 = self.getSURFFeatures(i1)
+		imageSet2 = self.getSURFFeatures(i2)
+		print ("Direction : ", direction)
+		matches = self.flann.knnMatch(
 			imageSet2['des'],
 			imageSet1['des'],
 			k=2
 			)
-		good = []		#for keeping the good features
+		good = []
 		for i , (m, n) in enumerate(matches):
 			if m.distance < 0.7*n.distance:
 				good.append((m.trainIdx, m.queryIdx))
@@ -44,7 +38,7 @@ class matchers:
 			return H
 		return None
 
-	def getFeatures(self, im):
+	def getSURFFeatures(self, im):
 		gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-		kp, des = self.orb.detectAndCompute(gray, None)
+		kp, des = self.surf.detectAndCompute(gray, None)
 		return {'kp':kp, 'des':des}
